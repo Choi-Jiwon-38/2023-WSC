967c3ec3ee912dd64d9c81356a87f18fb5479b3a
죄죄원 <81795729+Choi-Jiwon-38@users.noreply.github.com>
2023-06-05 22:04:38 +0900

    feat: django rest framework 설치 및 추가

--- config/config/urls.py
+++ config/config/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include # include 빼먹지 않고 추가하기!
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')) # 추가
 ]
