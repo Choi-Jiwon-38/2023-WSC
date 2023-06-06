11264e29ec44a4f4c8fd39589dffc9d86909f98e
죄죄원 <81795729+Choi-Jiwon-38@users.noreply.github.com>
2023-06-06 12:14:13 +0900

    remove: django-rest 관련 프로젝트 삭제

--- config/myapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import include, path
-from rest_framework.routers import DefaultRouter
-from .import views
-
-router = DefaultRouter()
-router.register('post', views.PostViewSet)
-urlpatterns = [
-    path('', include(router.urls)),
-]
